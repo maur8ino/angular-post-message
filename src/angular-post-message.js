f706dc666270b1bd546720d7f7f95b26d9d3dab9
Mauro Verrocchio <mauro@gild.com>
2015-05-20 17:51:02 +0200

    adds a provider for configuring show console and renames service

--- src/angular-post-message.js
+++ src/angular-post-message.js
@@ -2,11 +2,24 @@
   'use strict';
   var app;
 
-  app = angular.module("ngPostMessage", ['ng']);
+  app = angular.module('ngPostMessage', ['ng']);
+
+  app.provider('$postMessage', function() {
+    var $postMessageProvider = {
+      options: {
+        showConsole: false
+      },
+      $get: function() {
+        return angular.extend({}, $postMessageProvider.options);
+      }
+    };
+
+    return $postMessageProvider;
+  });
 
   app.run([
-    '$window', '$postMessage', '$rootScope',
-    function($window, $postMessage, $rootScope) {
+    '$window', '$rootScope', '$postMessage', 'postMessageService',
+    function($window, $rootScope, $postMessage, postMessageService) {
 
       $rootScope.$on('$messageOutgoing', function(event, message, domain) {
         var sender;
@@ -18,26 +31,27 @@
       });
 
       angular.element($window).bind('message', function(event) {
-        var error, response;
+        var response;
         event = event.originalEvent || event;
         if (event && event.data) {
           response = null;
           $rootScope.sender = event.source;
           try {
             response = angular.fromJson(event.data);
-          } catch (_error) {
-            error = _error;
-            console.error('ahem', error);
+          } catch (error) {
+            if ($postMessage.showConsole) {
+              console.error('ahem', error);
+            }
             response = event.data;
           }
           $rootScope.$root.$broadcast('$messageIncoming', response);
-          return $postMessage.messages(response);
+          return postMessageService.messages(response);
         }
       });
     }
   ]);
 
-  app.factory("$postMessage", [
+  app.factory('postMessageService', [
     '$rootScope',
     function($rootScope) {
       var $messages, api;
